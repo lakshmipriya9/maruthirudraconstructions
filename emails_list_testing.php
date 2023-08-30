b40cf7139bb292b134ba986146acf07c20b35077
lakshmipriya9 <57885771+lakshmipriya9@users.noreply.github.com>
2023-08-30 11:49:13 +0530

    Add files via upload

---
+++ emails_list_testing.php
@@ -0,0 +1,30 @@
+<?php
+
+
+
+$from = 'lakshmipriyauv99@gmail.com';
+
+$from_name = 'Maruthi Rudra Constructions';
+
+
+
+// $to = 'sales@rrinfra.com';
+$to = 'lakshmipriyauv99@gmail.com';
+
+$to_name = 'Maruthi Rudra Constructions';
+
+
+
+$cc_emails = array(
+	
+// 'sunil@rrinfra.com','nithyasree@deepredink.com'
+);
+
+
+
+$bcc_emails = array(
+
+
+
+);
+
